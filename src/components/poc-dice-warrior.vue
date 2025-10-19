60d66bbc3ca7d365064f821b8b4e911a51d46c76
Guillaume MANSUTTI <GuiguiLeGolem@guillaume.mansutti10@outlook.fr>
2025-10-19 18:03:41 +0200

    commencer à faire de la merde avec poc-dice-warrior

---
+++ src/components/poc-dice-warrior.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Oe</h1>
+    <!-- Affiche l'objet entier en JSON -->
+    <pre>{{ JSON.stringify(monObjet, null, 2) }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+// Création d'un objet complexe
+const monObjet = reactive({
+  class: {
+    name: "Warrior",
+    dices: [
+      { values: [1, 2, 3, 4, 5, 6] },
+      { values: [1, 2, 3, 4, 5, 6] },
+      { values: [1, 2, 3, 4, 5, 6] },
+      { values: [1, 2, 3, 4, 5, 6] },
+      { values: [1, 2, 3, 4, 5, 6] },
+    ],
+    skills: [
+      { name: "", degats: 25 }
+    ]
+  },
+  cards: {
+
+  }
+})
+</script>
